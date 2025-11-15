4e66b8a2baaa6d8b4fe6156b4b9e502a70b67d86
Jonathan Bodnar <jonathanbodnar@gmail.com>
2025-11-15 14:45:03 -0600

    fix: Add autocomplete attributes to login form inputs

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -80,6 +80,7 @@ export default function Login() {
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                autoComplete="email"
                 required
                 style={{
                   width: '100%',
@@ -99,6 +100,7 @@ export default function Login() {
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
                 required
                 style={{
                   width: '100%',
